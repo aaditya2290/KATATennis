883cf79ef0c036f35a2586ea7efc76caf0e0c91e
aaditya2290 <aaditya_2290@yahoo.co.in>
2019-09-04 16:50:11 +0530

    Proper package structure, remove comments, correct indentation

--- KATATennisGameProblem/src/main/TennisGame.java
+++ KATATennisGameProblem/src/main/TennisGame.java
@@ -1,105 +1,106 @@
+package main;
 
 public class TennisGame {
-	
+
 	private String firstPlayerName;
 	private String secondPlayerName;
 	private Integer firstPlayerScore=0;
 	private Integer secondPlayerScore=0;
 
-	TennisGame(String firstPlayer,String secondPlayer)
+	public TennisGame(String firstPlayer,String secondPlayer)
 	{
 		this.firstPlayerName=firstPlayer;
 		this.secondPlayerName=secondPlayer;
-				
+
 	}
-	
+
 	public String getGameScore()
 	{
-		if (firstPlayerScore==0 && secondPlayerScore==0) //score at start of game
-		return "Love All";
+		if (firstPlayerScore==0 && secondPlayerScore==0)
+			return "Love All";
 		else 
 		{
-		if (isInvalid())
-		return "Invalid input scores for game";
-		else if (playerWinsGame()) // score if one player has won game
-		{
-			if (firstPlayerScore>secondPlayerScore)
-			return getFirstPlayerName()+" wins";
+			if (isInvalid())
+				return "Invalid input scores for game";
+			else if (playerWinsGame())
+			{
+				if (firstPlayerScore>secondPlayerScore)
+					return getFirstPlayerName()+" wins";
+				else
+					return getSecondPlayerName()+" wins";
+			}
+			else if (isAdvantage())
+			{
+				if (firstPlayerScore>secondPlayerScore)
+					return "Advantage "+getFirstPlayerName();
+				else
+					return "Advantage "+getSecondPlayerName();
+			}
+			else if (isDeuce())
+				return "Deuce";
+			else if (firstPlayerScore==secondPlayerScore)
+				return calculateScore(firstPlayerScore)+" All";
 			else
-			return getSecondPlayerName()+" wins";
+				return calculateScore(firstPlayerScore)+"-"+calculateScore(secondPlayerScore);
 		}
-		else if (isAdvantage()) //score in case of advantage
-		{
-			if (firstPlayerScore>secondPlayerScore)
-			return "Advantage "+getFirstPlayerName();
-			else
-			return "Advantage "+getSecondPlayerName();
-		}
-		else if (isDeuce())
-		return "Deuce";
-		else if (firstPlayerScore==secondPlayerScore) // score in case of equal ball scores
-		return calculateScore(firstPlayerScore)+" All";
-		else
-		return calculateScore(firstPlayerScore)+"-"+calculateScore(secondPlayerScore);
-		}
-				
+
 	}
-	
+
 	public String calculateScore(Integer score)
 	{
 		if (score==0)
-		return "Love";
+			return "Love";
 		else if (score==1)
-		return "Fifteen";
+			return "Fifteen";
 		else if (score==2)
-		return "Thirty";
+			return "Thirty";
 		else if (score==3)
-		return "Forty";
+			return "Forty";
 		else return "Invalid score";
-			
+
 	}
 	public boolean isInvalid()
 	{
 		return (firstPlayerScore>4 && firstPlayerScore>secondPlayerScore+2)|| (secondPlayerScore>4 && secondPlayerScore>firstPlayerScore+2);
-			
+
 	}
-	
+
 	public boolean isDeuce()
 	{
 		return (firstPlayerScore>=3 && firstPlayerScore==secondPlayerScore);
-			
+
 	}
-	
+
 	public boolean isAdvantage()
 	{
 		return (firstPlayerScore>3 && firstPlayerScore==secondPlayerScore+1)||(secondPlayerScore>3 && (secondPlayerScore==firstPlayerScore+1));
-			
+
 	}
-	
+
 	public boolean playerWinsGame()
 	{
 		return (firstPlayerScore>=4 && firstPlayerScore>=secondPlayerScore+2)||(secondPlayerScore>=4 && secondPlayerScore>=firstPlayerScore+2);
-			
+
 	}
-	
+
 	public void firstPlayerWinsBall()
 	{
 		firstPlayerScore++;	
 	}
-	
+
 	public void secondPlayerWinsBall()
 	{
 		secondPlayerScore++;	
 	}
-	
+
 	public void resetScores()
 	{
 		firstPlayerScore=0;
 		secondPlayerScore=0;
 	}
-	
-	
-	
+
+
+
 	public String getFirstPlayerName() {
 		return firstPlayerName;
 	}
--- KATATennisGameProblem/src/test/TennisGameTest.java
+++ KATATennisGameProblem/src/test/TennisGameTest.java
@@ -1,35 +1,39 @@
+package test;
 import static org.junit.Assert.*;
+import main.TennisGame;
 
+import org.junit.Before;
 import org.junit.Test;
 
 
 public class TennisGameTest {
-	
+
 	TennisGame game;
-	
-	public TennisGameTest()
+
+	@Before
+	public void initializeTennisGame()
 	{
-	   game=new TennisGame("Player-1","Player-2");
+		game=new TennisGame("Player-1","Player-2");	
 	}
 
 	@Test
 	public void testInitialGameScore() {
 		assertEquals("Love All",game.getGameScore());
 	}
-	
+
 	@Test
 	public void testPlayerWinsBall() {
 		game.firstPlayerWinsBall();
 		assertEquals("Fifteen-Love",game.getGameScore());
 	}
-	
+
 	@Test
 	public void testPlayersWinEqualBalls() {
 		game.firstPlayerWinsBall();
 		game.secondPlayerWinsBall();
 		assertEquals("Fifteen All",game.getGameScore());
 	}
-	
+
 	@Test
 	public void testPlayerWinsGame() {
 		createGame(4,1);
@@ -37,9 +41,9 @@ public class TennisGameTest {
 		game.resetScores();
 		createGame(2,4);
 		assertEquals("Player-2 wins",game.getGameScore());
-		
+
 	}
-	
+
 	@Test
 	public void testAdvantage() {
 		createGame(3,4);
@@ -47,9 +51,9 @@ public class TennisGameTest {
 		game.resetScores();
 		createGame(5,4);
 		assertEquals("Advantage Player-1",game.getGameScore());
-			
+
 	}
-	
+
 	@Test
 	public void testDeuce() {
 		createGame(3,3);
@@ -57,9 +61,9 @@ public class TennisGameTest {
 		game.resetScores();
 		createGame(5,5);
 		assertEquals("Deuce",game.getGameScore());
-			
+
 	}
-	
+
 	@Test
 	public void testInvalidGame() {
 		createGame(10,3);
@@ -67,15 +71,15 @@ public class TennisGameTest {
 		game.resetScores();
 		createGame(5,8);
 		assertEquals("Invalid input scores for game",game.getGameScore());
-			
+
 	}
-	
+
 	public void createGame(Integer firstPlayerScore,Integer secondPlayerScore)
 	{
 		for (int i=0;i<firstPlayerScore;i++)
-		game.firstPlayerWinsBall();
+			game.firstPlayerWinsBall();
 		for (int i=0;i<secondPlayerScore;i++)
-		game.secondPlayerWinsBall();	
+			game.secondPlayerWinsBall();	
 	}
 
 }
